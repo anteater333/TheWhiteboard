307f989beb05f46dd986c93a2b03d16271b9f872
Ji-hoon Lee <anteater1056@gmail.com>
2022-08-07 17:58:48 +0900

    Hello route

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,53 @@
-import React from "react";
-import { GoogleOAuthProvider, GoogleLogin } from "@react-oauth/google";
+import React, { useCallback } from "react";
+import {
+  GoogleOAuthProvider,
+  GoogleLogin,
+  CredentialResponse,
+} from "@react-oauth/google";
 import "./App.scss";
+import { Route, Routes, useNavigate } from "react-router-dom";
+import Whiteboard from "./whiteboard";
 
 const clientId =
   "706303852091-uqhi736g8tqcm4un3s2usu1qa0t0avdb.apps.googleusercontent.com";
 
 function App() {
   return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/whiteboard" element={<Whiteboard />} />
+      </Routes>
+    </div>
+  );
+}
+
+function Home() {
+  const navigate = useNavigate();
+  const handleOnLoginSuccess = useCallback(
+    (credential: CredentialResponse) => {
+      console.log(credential);
+      navigate("/whiteboard");
+    },
+    [navigate]
+  );
+  return (
     <GoogleOAuthProvider clientId={clientId}>
-      <div className="App">
-        <header className="App-header">
-          <div className="App-title">
-            <label className="title-the unselectable">{"The"}</label>
-            <label className="title-whiteboard unselectable">
-              {"Whiteboard"}
-            </label>
-          </div>
-        </header>
-        <div className="App-content">
-          <GoogleLogin
-            onSuccess={(credentialResponse) => {
-              console.log(credentialResponse);
-            }}
-            onError={() => {
-              console.log("Login Failed");
-            }}
-            useOneTap
-          />
+      <header className="App-header">
+        <div className="App-title">
+          <label className="title-the unselectable">{"The"}</label>
+          <label className="title-whiteboard unselectable">
+            {"Whiteboard"}
+          </label>
         </div>
+      </header>
+      <div className="App-content">
+        <GoogleLogin
+          onSuccess={handleOnLoginSuccess}
+          onError={() => {
+            console.log("Login Failed");
+          }}
+        />
       </div>
     </GoogleOAuthProvider>
   );
---
+++ src/whiteboard/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Memo from "./Memo";
+import "./Whiteboard.scss";
+
+function Whiteboard() {
+  return (
+    <div className="whiteboard">
+      <Memo />
+    </div>
+  );
+}
+
+export default Whiteboard;
