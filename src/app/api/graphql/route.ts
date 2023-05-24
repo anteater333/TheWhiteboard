d4e384534eac49147678e9b9ed1c25b3193ffa7e
Ji-hoon Lee <anteater1056@gmail.com>
2023-05-24 18:20:38 +0900

    GraphQL Server Setting

---
+++ src/app/api/graphql/route.ts
@@ -0,0 +1,56 @@
+import { startServerAndCreateNextHandler } from "@as-integrations/next";
+import { ApolloServer } from "@apollo/server";
+import {
+  GraphQLID,
+  GraphQLObjectType,
+  GraphQLSchema,
+  GraphQLString,
+} from "graphql";
+import { NextRequest } from "next/server";
+
+const UserType = new GraphQLObjectType({
+  name: "User",
+  fields: {
+    id: { type: GraphQLID },
+    oauth_id: { type: GraphQLID },
+    nickname: { type: GraphQLString },
+    created_at: { type: GraphQLString },
+    updated_at: { type: GraphQLString },
+  },
+});
+
+const Query = new GraphQLObjectType({
+  name: "Query",
+  fields: {
+    user: {
+      type: UserType,
+      resolve: (_, args) => {
+        return [
+          {
+            id: "id",
+            oauth_id: "oauth_id",
+            nickname: "nickname",
+            created_at: "created_at",
+            updated_at: "updated_at",
+          },
+        ];
+      },
+    },
+  },
+});
+
+const server = new ApolloServer({
+  schema: new GraphQLSchema({ types: [UserType], query: Query }),
+});
+
+const handler = startServerAndCreateNextHandler<NextRequest>(server, {
+  context: async (req) => ({ req }),
+});
+
+export async function GET(request: Request) {
+  return handler(request);
+}
+
+export async function POST(request: Request) {
+  return handler(request);
+}
--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
 
 export async function GET(_request: Request) {
-  return NextResponse.json({ hello: "world" });
+  return NextResponse.json({ hello: `world. This is ${Date.now()}` });
 }
