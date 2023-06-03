3ab470476b440482d5f97a2f287b3f9ddfe3fa93
Ji-hoon Lee <anteater1056@gmail.com>
2023-06-03 21:59:20 +0900

    not-found 화면 스타일 적용

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,23 +2,13 @@
 
 import Link from "next/link";
 
-// const Container = styled.div``;
-
-// const Title = styled.h1`
-//   font-family: "Galmuri11";
-//   font-weight: normal;
-// `;
-
-// const TurningBack = styled.div`
-//   font-family: "Galmuri11";
-//   font-weight: normal;
-// `;
-
 export default function NotFound() {
   return (
-    <div>
-      <h1>존재하지 않는 페이지 입니다.</h1>
-      <div>
+    <div className="flex h-screen w-screen flex-col items-center justify-center font-galmuri">
+      <h1 className="-mt-16 text-2xl font-bold">
+        존재하지 않는 페이지 입니다.
+      </h1>
+      <div className="mt-4 text-xl hover:underline">
         <Link href="/">홈페이지로 돌아가기</Link>
       </div>
     </div>
