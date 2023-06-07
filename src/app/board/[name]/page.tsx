94ccabcc6da1e6413d663fcf965aafafbc4d9290
Ji-hoon Lee <anteater1056@gmail.com>
2023-06-07 09:03:55 +0900

    Route 파라미터 사용

--- src/app/board/[name]/page.tsx
+++ src/app/board/[name]/page.tsx
@@ -4,8 +4,16 @@ import { AddButton, OnGoingMemoButton } from "@/components/buttons.component";
 import { authOptions } from "@/service/auth";
 import { getServerSession } from "next-auth";
 
-export default async function BoardPage() {
+export default async function BoardPage({
+  params,
+  searchParams,
+}: {
+  params: { name: string };
+  searchParams: { [key: string]: string | string[] | undefined };
+}) {
   const session = await getServerSession(authOptions);
+
+  const pageNum = searchParams["page"] ?? 1;
 
   return (
     <div className="flex h-screen w-screen flex-col">
@@ -14,28 +22,28 @@ export default async function BoardPage() {
         className="flex justify-center border-b-2 border-gray-200 bg-stone-100 py-2"
       >
         <div id="board-title" className="font-galmuri text-3xl font-bold">
-          world
+          {params.name}
         </div>
       </div>
       <div id="board-center" className="relative h-full w-full">
         <Board></Board>
-        <div className="absolute bottom-8 right-8 w-fit">
+        <div className="absolute bottom-4 right-4 w-fit">
           <AddButton />
         </div>
-        <div className="absolute bottom-8 left-8 w-fit">
+        <div className="absolute bottom-4 left-4 w-fit">
           <OnGoingMemoButton />
         </div>
       </div>
       <div
         id="board-footer"
-        className="flex w-full items-center border-t-2 border-gray-200 bg-stone-100 px-8 py-4 font-galmuri font-bold"
+        className="flex w-full items-center border-t-2 border-gray-200 bg-stone-100 px-8 py-1 font-galmuri font-bold"
       >
         <div className="flex flex-1 justify-center">
           <LogoutButton className="mr-auto text-2xl hover:underline" />
         </div>
         <div className="flex flex-1 justify-center gap-2 text-3xl leading-normal">
           <span>{`<`}</span>
-          <span>1</span>
+          <span>{pageNum}</span>
           <span>{`>`}</span>
         </div>
         <div className="flex flex-1 justify-center overflow-visible">
