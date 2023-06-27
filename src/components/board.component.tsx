540d9c4bd9e76c9a310983b287cc49a6f5e112a9
Ji-hoon Lee <anteater1056@gmail.com>
2023-06-27 20:33:03 +0900

    원시적인 드래그 & 드롭 로직
    리펙토링 및 튜닝 필요

--- src/components/board.component.tsx
+++ src/components/board.component.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useCallback, useState, WheelEvent } from "react";
+import { useCallback, useRef, useState, WheelEvent } from "react";
 import { Memo } from "./memo.component";
 
 const numOfLevels = 8;
@@ -13,12 +13,16 @@ const minScale = maxScale / numOfLevels;
  * @returns
  */
 export const Board = function () {
+  const boardRef = useRef<HTMLDivElement | null>(null);
+
   const [scaleLevel, setScaleLevel] = useState(4);
   const [scale, setScale] = useState(scaleLevel * minScale);
   const [posX, setPosX] = useState(0);
   const [posY, setPosY] = useState(0);
-  const [mouseX, setMouseX] = useState(0);
-  const [mouseY, setMouseY] = useState(0);
+  const [startMouseX, setStartMouseX] = useState(0);
+  const [startMouseY, setStartMouseY] = useState(0);
+
+  const [isDragging, setIsDragging] = useState(false);
 
   /** Board 영역을 확대/축소 */
   const handleOnWheel = useCallback(
@@ -46,12 +50,31 @@ export const Board = function () {
     <div
       className="absolute z-0 h-screen w-screen bg-sky-500 transition-transform"
       onWheel={handleOnWheel}
+      onMouseDown={(event) => {
+        setStartMouseX(event.clientX);
+        setStartMouseY(event.clientY);
+        setIsDragging(true);
+      }}
+      onMouseUp={(event) => {
+        setIsDragging(false);
+      }}
       onMouseMove={(event) => {
-        setMouseX(event.clientX);
-        setMouseY(event.clientY);
+        if (!isDragging) {
+          return;
+        }
+
+        setPosX((prev) => {
+          return prev - (startMouseX - event.clientX) / scaleLevel;
+        });
+        setPosY((prev) => {
+          return prev - (startMouseY - event.clientY) / scaleLevel;
+        });
+
+        setStartMouseX(event.clientX);
+        setStartMouseY(event.clientY);
       }}
       style={{
-        transform: `scale(${scale})`,
+        transform: `scale(${scale}) translate(${posX}px, ${posY}px)`,
       }}
     >
       <div className="fixed left-1/2 top-96 z-50 text-9xl">{scaleLevel}</div>
